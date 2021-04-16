c21b715735998820e8b7fbd0f0bd883cb359b492
unknown <qq844626148@163.com>
2021-04-16 21:24:34 +0800

    完成了首页的功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,34 @@
 // jQuery的内置方法ajaxPrefilter
 // 每次发起$.get(),$.post()或$.ajax()请求前都会调用此函数
+// 统一配置 ajax 对象
 $.ajaxPrefilter(function(options) {
     // options是发送请求的配置对象
     // 拼接根地址
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    // 判断访问的是否是有权限的接口，有选择性的发送 headers
+    // indexOf() 可以用来判断字符串内是否含有指定的子串，
+    // 找得到就返回一个实数，找不到则返回 -1
+    if(options.url.indexOf('/my' !== -1)) {
+        // 访问的是有权限的接口
+        // 因为请求的是有权限访问的接口，故需配置headers请求头的Authorization属性，
+        // 发送token，用于身份认证，从浏览器的 localStorage 中获取服务端发来的 token 字段
+        // 找不到则默认发送空字符串
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 访问的不是有权限的接口，无需配置请求头
+
+    // jQuery的ajax请求有三种回调函数，
+        // success(成功执行), error(错误执行), complete(请求完成执行，不管成功与否)
+        // 在 complete 回调函数内判断用户的登陆状态，禁止用户未登录访问后台页面
+        options.complete = (res) => {
+            // complete 回调函数的返回参数中的 responseJSON 属性传递了信息对象
+            if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 1.1 清空token字符串
+                localStorage.removeItem('token');
+                // 1.2 强制跳转回login页面
+                location.href = '/login.html';
+            }
+        }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,76 @@
+// 主页的js文件
+// $(function() {}) 等价于 document.onready(){} 事件，
+// 即当页面的 DOM 元素加载完成后就自动执行该函数
+$(function() {
+    // 调用获取用户信息的函数
+    getUserInfo();
+    
+    // 给退出按钮绑定点击事件，退出登陆
+    $('#btnLogout').on('click', function() {
+        // 在 layui 中调用 layer 模块
+        layui.use('layer', function(){
+            var layer = layui.layer;
+            
+            // 使用弹出layer的提示对话框
+            layer.confirm('是否确认退出?', {icon: 3, title:'提示'}, function(index){
+                // 点击确定按钮触发此函数
+                // 1.1 删除 localStorage 内的token 字符串
+                localStorage.removeItem('token');
+                // 1.2 跳转到login.html页面
+                location.href = '/login.html';
+
+                // 询问框的默认关闭事件
+                layer.close(index);
+            })
+        })
+        
+    })
+        
+})
+
+// 定义获取用户信息的函数
+function getUserInfo() {
+    // 发起获取用户基本信息的请求
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: (res) => {
+            // 判断用户的登陆状态
+            if(res.status !== 0) {
+                // 通过 layui 弹出提示消息
+                return layui.use('layer', function(){
+                    var layer = layui.layer;
+                    
+                    layer.msg('获取用户信息失败！');
+                  });
+            }
+            // 获取用户信息成功，调用渲染用户用户名和头像的函数
+            renderAvatarAndUsername(res.data);
+        }
+    })
+}
+
+// 定义渲染用户头像和用户名的函数
+function renderAvatarAndUsername(user) {
+    // 1.渲染用户名
+    // 1.1 判断用户是否有nickname，有就渲染，无就显示username
+    const name = user.nickname || user.username;
+    $('#welcome-user').html(`${name}  欢迎您！`)
+    // 2.渲染用户头像
+    // 判断用户是否有自定义头像，有就显示，无就渲染文本头像
+    if(user.user_pic !== null) {
+        // 2.1 用户的自定义头像属性不为空，渲染用户自定义头像
+        // 2.1.1 设置用户自定义头像的src属性并显示
+        $('.user-avatar').attr('src',user.user_pic).show();
+        // 2.1.2 隐藏文本头像
+        $('.text-avatar').hide();
+        return
+    }
+    // 2.2 用户自定义属性为空，渲染文本头像
+    // 2.2.1 获取用户的用户名，提取第一个字符并大写显示
+    const first = user.username.slice(0,1).toUpperCase();
+    // 2.2.2 将大写的首字母渲染到文本头像内,并显示文本头像
+    $('.text-avatar').html(first).show();
+    // 2.2.3 隐藏用户自定义头像
+    $('.user-avatar').hide();
+}
